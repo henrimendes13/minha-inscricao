9a4408e5cd64668e8af4b279c1c62e24ca4ce533
Henrique Mendes <henriquemendes13@gmail.com>
2025-08-30 12:11:22 -0300

    1. Dependências

--- src/main/java/br/com/eventsports/minha_inscricao/util/PasswordUtil.java
+++ src/main/java/br/com/eventsports/minha_inscricao/util/PasswordUtil.java
@@ -1,21 +1,24 @@
 package br.com.eventsports.minha_inscricao.util;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PasswordUtil {
 
+    private final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
     /**
-     * Retorna a senha sem codificação (security removed)
+     * Codifica a senha usando BCrypt
      */
     public String encode(String rawPassword) {
-        return rawPassword;
+        return encoder.encode(rawPassword);
     }
 
     /**
-     * Verifica se duas senhas são iguais
+     * Verifica se a senha raw corresponde à senha codificada
      */
     public boolean matches(String rawPassword, String encodedPassword) {
-        return rawPassword.equals(encodedPassword);
+        return encoder.matches(rawPassword, encodedPassword);
     }
 }
